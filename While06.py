251aa3f6783f744b0a6e507f77674b1bb2c694c8
abdusalom <boriboyevabdusalom@gmail.com>
2023-11-17 10:19:22 +0500

    6

--- While06.py
+++ While06.py
@@ -7,3 +7,11 @@ def main(s):
     Returns:
         int: return answer
     """
+    i = 0
+    consonat = 0
+    while i<len(s):
+        if s[i]=="a" or s[i]=="e" or s[i]=="i" or s[i]=="o" or s[i]=="u" or s[i]=="o'":
+            consonat +=1
+        i+=1
+    return len(s)-consonat -1
+print(main("CodeschoolUz"))
